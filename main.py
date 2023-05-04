46d97575495e8014d112d04752afcb1b895ce7cd
Che4kaa <mmegayashchikov@bk.ru>
2023-05-04 19:53:58 +0300

    The ask_password() function, which asks the user for a password and checks it with a string that says: "Enter your password". The user is given 3 attempts, as soon as it matches the correct value, the function should output "Password accepted" and ignore further input. If the user failed to guess the password after three attempts, the function should display: "Access denied" and ignore further input.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-student = ("Michael", "20", "excellent student")
+password = 'Your password'
 
-print(student.count("Michael"))
-print(student.index("excellent student"))
-
-for x in student:
-    print(x)
-
-if "Michael" in student:
-    print("Michael is here!")
+def ask_passsword():
+    a = input()
+    if a == 'Your password':
+        print(password)
+for i in reversed(range(3)):
+    a = input("Enter the password: ")
+    if a == 'Your password':
+        print("Password accepted ")
+        break
+else:
+    print("Access denied ")
